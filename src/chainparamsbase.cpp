252fb030453d1b6b9b6b7764657b91240f6a3e12
BitcoinKnotsCash <dev@bitcoinknotscash.org>
2026-09-26 15:36:52 +0100

    Set XBTC default RPC ports (8556 mainnet, avoid Bitcoin 8332 collision)

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -44,15 +44,15 @@ std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const ChainType chain)
 {
     switch (chain) {
     case ChainType::MAIN:
-        return std::make_unique<CBaseChainParams>("", 8332);
+        return std::make_unique<CBaseChainParams>("", 8556);
     case ChainType::TESTNET:
-        return std::make_unique<CBaseChainParams>("testnet3", 18332);
+        return std::make_unique<CBaseChainParams>("testnet3", 18556);
     case ChainType::TESTNET4:
         return std::make_unique<CBaseChainParams>("testnet4", 48332);
     case ChainType::SIGNET:
         return std::make_unique<CBaseChainParams>("signet", 38332);
     case ChainType::REGTEST:
-        return std::make_unique<CBaseChainParams>("regtest", 18443);
+        return std::make_unique<CBaseChainParams>("regtest", 18557);
     }
     assert(false);
 }
